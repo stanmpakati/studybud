6054291fe8b77482efe90bed3b56c7488342b4da
stan000 <stanmpakatig@gmail.com>
2021-10-14 16:09:30 +0200

    externalised routes

--- base/views.py
+++ base/views.py
@@ -1,3 +1,10 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(req):
+    return HttpResponse('Home')
+
+def room(req):
+    return HttpResponse('Room')
