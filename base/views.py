5a4ffd1a819e40b24f90c0737cfa6323eac36226
stan000 <stanmpakatig@gmail.com>
2021-10-15 18:30:33 +0200

    template injection

--- base/views.py
+++ base/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, 'name': 'Fine Django'},
+    {'id': 2, 'name': 'Yey Flutter'},
+    {'id': 3, 'name': 'Yey Angular'},
+]
+
 def home(req):
-    return render(req, 'home.html')
+    return render(req, 'home.html', {'rooms': rooms})
 
 def room(req):
     return render(req, 'room.html')
