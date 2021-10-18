6acb9d726d1fd0eb5200633363ba455f3f4707bd
stan000 <stanmpakatig@gmail.com>
2021-10-18 19:10:00 +0200

    passing data to room

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 rooms = [
-    {'id': 1, 'name': 'Fine Django'},
+    {'id': 1, 'name': 'Fine I\'ll learn Django'},
     {'id': 2, 'name': 'Yey Flutter'},
     {'id': 3, 'name': 'Yey Angular'},
 ]
@@ -9,5 +9,12 @@ rooms = [
 def home(req):
     return render(req, 'base/home.html', {'rooms': rooms})
 
-def room(req):
-    return render(req, 'base/room.html')
+def room(req, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}
+
+    return render(req, 'base/room.html', context)
