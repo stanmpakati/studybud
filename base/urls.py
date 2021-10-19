d840812862cd9e1d174de54c69099e75206badae
stan000 <stanmpakatig@gmail.com>
2021-10-19 16:20:14 +0200

    update form

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('room/', views.room, name='rooms'),
-    path('room/<str:pk>', views.room, name='room'),
-    path('create-room/', views.createRoom, name='create-room')
+    path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>/', views.updateRoom, name='update-room')
 ]
--- base/views.py
+++ base/views.py
@@ -23,3 +23,16 @@ def createRoom(req):
 
     context={'form': form}
     return render(req, 'base/room_form.html', context)
+
+def updateRoom(req, pk):
+    room = Room.objects.get(id = pk)
+    form = RoomForm(instance=room)
+
+    if req.method == 'POST':
+        form = RoomForm(req.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render (req, 'base/room_form.html', context)
