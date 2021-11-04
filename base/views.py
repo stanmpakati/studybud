1e402082ddf031ef878c1691c922f17a5e41f4e7
stan000 <stanmpakatig@gmail.com>
2021-11-04 15:54:48 +0200

    Added to search params

--- base/views.py
+++ base/views.py
@@ -1,11 +1,16 @@
 from django.http import request
+from django.db.models import Q
 from django.shortcuts import redirect, render
 from .models import Room, Topic
 from .forms import RoomForm
 
 def home(req):
     q = req.GET.get('q') if req.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
     topics = Topic.objects.all()
 
     context = {'rooms': rooms, 'topics': topics}
